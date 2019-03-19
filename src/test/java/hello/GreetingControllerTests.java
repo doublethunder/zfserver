2304cba52004f37bbc97b90df3507950bc9a012b
chLei <993716335@qq.com>
2019-03-19 15:43:00 +0800

    增加log4j2配置

--- src/test/java/hello/GreetingControllerTests.java
+++ src/test/java/hello/GreetingControllerTests.java
@@ -16,19 +16,12 @@
 package hello;
 
 import com.dt.miniapp.Application;
-import com.dt.miniapp.system.Props;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 

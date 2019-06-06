1965b807e44e30e8c73a9aabf2964f7855fee676
陈蕾 <993716335@qq.com>
2019-06-06 23:23:23 +0800

    再教一次
    再教一次

---
+++ src/main/java/com/dt/miniapp/Test1.java
@@ -0,0 +1,12 @@
+package com.dt.miniapp;
+
+/**
+ * @author chenlei
+ * @description
+ * @date 2019-05-15 08:38
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.printf("123333xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    }
+}
